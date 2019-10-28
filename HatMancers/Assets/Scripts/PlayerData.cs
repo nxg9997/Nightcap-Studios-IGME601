f53479a2512fed4e8cf5b3e778850d5ead79f8f6
nxg9997 <nxg9997@rit.edu>
2019-10-28 17:27:11 -0400

    fireballs and some useful scripts

---
+++ HatMancers/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject currHat;
+
+    public string currMagic = "none";
+
+    public int health = 100;
+
+    public GameObject spawn;
+    private float spawnTimer = 0;
+    public float spawnTime = 3;
+    public bool dead = false;
+
+    public bool testDummy = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (testDummy)
+        {
+            Destroy(GetComponentInChildren<Camera>());
+            Destroy(GetComponentInChildren<PlayerController>());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dead)
+        {
+            Respawn();
+        }
+        else
+        {
+            StickyHat();
+            CheckHealth();
+        }
+    }
+
+    void UpdateMagic()
+    {
+        if(currHat.tag == "FireHat")
+        {
+            currMagic = "fire";
+        }
+    }
+
+    void StickyHat()
+    {
+        if (currHat == null) return;
+        currHat.transform.position = GameObject.Find("HatLocation").transform.position;
+        currHat.transform.rotation = GameObject.Find("HatLocation").transform.rotation;
+    }
+
+    void CheckHealth()
+    {
+        if(health <= 0)
+        {
+            dead = true;
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    void Respawn()
+    {
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer > spawnTime)
+        {
+            dead = false;
+            GetComponent<MeshRenderer>().enabled = true;
+            transform.position = spawn.transform.position;
+            health = 100;
+            spawnTimer = 0;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag.Contains("Hat"))
+        {
+            if(currHat != null)
+            {
+                Destroy(currHat);
+            }
+            currHat = col.gameObject;
+            currHat.GetComponent<Collider>().enabled = false;
+            UpdateMagic();
+        }
+        else if(col.gameObject.tag == "Damage")
+        {
+            health -= col.gameObject.GetComponent<SpellData>().damage;
+        }
+    }
+}
---
+++ HatMancers/Assets/Scripts/Spells.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spells : MonoBehaviour
+{
+    public GameObject fireProj;
+    public float fireForce = 5.0f;
+    private bool fireDelay = false;
+    private float fireTime = 0.0f;
+    public float fireDelayTime = 2;
+
+    private PlayerData pData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pData = GetComponent<PlayerData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pData.testDummy)
+        {
+            CheckInput();
+        }
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetAxis("Fire1") > 0 && !fireDelay && pData.currMagic == "fire")
+        {
+            Fire();
+            fireDelay = true;
+            fireTime = 0;
+        }
+        else
+        {
+            fireTime += Time.deltaTime;
+            if(fireTime > fireDelayTime)
+            {
+                fireDelay = false;
+            }
+        }
+    }
+
+    void Fire()
+    {
+        GameObject fireball = GameObject.Instantiate(fireProj, transform.position + transform.forward * 2, Quaternion.identity);
+        fireball.transform.forward = gameObject.GetComponentInChildren<Camera>().transform.forward;
+        fireball.GetComponent<Rigidbody>().AddForce(fireball.transform.forward * fireForce);
+    }
+}
