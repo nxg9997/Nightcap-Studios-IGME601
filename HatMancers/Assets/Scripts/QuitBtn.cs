4b49d3ebb2e163d2b95ce584a2cf408226ba03c2
Nathan Glick <nxg9997@rit.edu>
2019-11-04 22:31:40 -0500

    added menu and quit option in game

---
+++ HatMancers/Assets/Scripts/QuitBtn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuitBtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button self = GetComponent<Button>();
+        self.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnClick()
+    {
+        Application.Quit();
+    }
+}
---
+++ HatMancers/Assets/Scripts/StartBtn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartBtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button self = GetComponent<Button>();
+        self.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClick()
+    {
+        //Debug.Log("testing");
+        SceneManager.LoadScene(1);
+    }
+}
